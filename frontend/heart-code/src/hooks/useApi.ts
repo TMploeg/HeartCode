bb91a23d09e9c9ecbd6187d34cbc3ff0787fc449
TMploeg <82577971+TMploeg@users.noreply.github.com>
2024-08-01 15:49:32 +0200

    fixed api url

--- frontend/heart-code/src/hooks/useApi.ts
+++ frontend/heart-code/src/hooks/useApi.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-//const API_URL = "http://localhost:8080/api/v1/"";
-const API_URL = "http://192.168.68.55:8080/api/v1/";
+const API_URL = "http://localhost:8080/api/v1/";
 
 export default function useApi() {
   function get<TResponse>(url: string) {
