5f323fed19a04d818340e7e91f091050478a509a
EGerritsen99 <145434680+EGerritsen99@users.noreply.github.com>
2024-08-02 14:23:37 +0200

    navbar

--- frontend/heart-code/src/App.tsx
+++ frontend/heart-code/src/App.tsx
@@ -1,14 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import MatchesPage from "./components/matches-page/MatchesPage";
+import NavigationBar from "./components/navigationbar/NavigationBar";
 import RegisterPage from "./components/auth/register-page/RegisterPage";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="" element={<div>Hello, HeartCode!</div>} />
-      <Route path="matches" element={<MatchesPage />} />
-      <Route path="register" element={<RegisterPage />} />
-    </Routes>
+    <div>
+      <Routes>
+        <Route path="" element={<div>Hello, HeartCode!</div>} />
+        <Route path="matches" element={<MatchesPage />} />
+        <Route path="register" element={<RegisterPage />} />
+      </Routes>
+      <NavigationBar />
+    </div>
   );
 }
