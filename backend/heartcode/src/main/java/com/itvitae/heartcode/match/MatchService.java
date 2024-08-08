ad794e84abfefd3accd6bf1a2073c3a220edc898
JavaGerben <154616858+JavaGerben@users.noreply.github.com>
2024-08-08 13:35:22 +0200

    rewrote code to meet meet the commented wishes

--- backend/heartcode/src/main/java/com/itvitae/heartcode/match/MatchService.java
+++ backend/heartcode/src/main/java/com/itvitae/heartcode/match/MatchService.java
@@ -5,6 +5,7 @@ import com.itvitae.heartcode.evaluation.EvaluationRepository;
 import com.itvitae.heartcode.user.User;
 import com.itvitae.heartcode.user.UserService;
 import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,20 +16,19 @@ public class MatchService {
   private final EvaluationRepository evaluationRepository;
   private final UserService userService;
 
-  public Match createMatch(User evaluator, User evaluatee, Boolean isLiked) {
-    if (isLiked && !getMatchedUsers(evaluator).contains(evaluatee)) {
-
-      var possibleEvaluation =
-          evaluationRepository.findByEvaluationIdEvaluatorAndEvaluationIdEvaluatee(
-              evaluatee, evaluator);
-      if (possibleEvaluation.isPresent()) {
-        Evaluation evaluation = possibleEvaluation.get();
-        if (evaluation.isLiked()) {
-          return matchRepository.save(new Match(evaluator, evaluatee));
-        }
-      }
+  public Optional<Match> createMatch(User evaluator, User evaluatee, Boolean isLiked) {
+    if (getMatchedUsers(evaluator).contains(evaluatee)) {
+      return Optional.empty();
     }
-    return null;
+
+    Optional<Evaluation> possibleEvaluation =
+        evaluationRepository.findByEvaluationIdEvaluatorAndEvaluationIdEvaluatee(
+            evaluatee, evaluator);
+
+    if (!isLiked || possibleEvaluation.filter(evaluation1 -> evaluation1.isLiked()).isEmpty()) {
+      return Optional.empty();
+    }
+    return Optional.of(matchRepository.save(new Match(evaluator, evaluatee)));
   }
 
   public List<Match> getAll() {
