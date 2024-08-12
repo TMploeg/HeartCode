c36214efc74b5374a1525c5540aa39d56bae10fd
TMploeg <82577971+TMploeg@users.noreply.github.com>
2024-08-12 13:30:08 +0200

    creaated update profile page

--- frontend/heart-code/src/components/profile-page/Profile.tsx
+++ frontend/heart-code/src/components/profile-page/Profile.tsx
@@ -1,6 +1,7 @@
 import { User } from "../../models/User";
 import { Card, Button } from "react-bootstrap";
 import "./Profile.css";
+import { Navigate, useNavigate } from "react-router-dom";
 
 interface Props {
   user: User;
@@ -8,6 +9,8 @@ interface Props {
 }
 
 export default function Profile({ user, isPersonalPage }: Props) {
+  const navigate = useNavigate();
+
   return (
     <div className="profile-page">
       <Card className="profile-card">
@@ -17,14 +20,20 @@ export default function Profile({ user, isPersonalPage }: Props) {
         />
         <Card.Body className="card-content">
           <Card.Title>{user.alias}</Card.Title>
-          <Card.Subtitle className="card-content">Gender, age</Card.Subtitle>
+          <Card.Subtitle className="">Gender, age</Card.Subtitle>
           <Card.Subtitle>Relationship type</Card.Subtitle>
           <Card className="bio">
             <Card.Text>Bio text goes here</Card.Text>
           </Card>
         </Card.Body>
         {/* Needs to navigate to edit profile page later*/}
-        {isPersonalPage ? <Button className="edit-button">Edit</Button> : <></>}
+        {isPersonalPage ? (
+          <Button className="edit-button" onClick={() => navigate("update")}>
+            Edit
+          </Button>
+        ) : (
+          <></>
+        )}
       </Card>
     </div>
   );
---
+++ frontend/heart-code/src/components/update-profile-page/UpdateProfilePage.tsx
@@ -0,0 +1,97 @@
+import { useNavigate } from "react-router-dom";
+import { useApi } from "../../hooks";
+import { useEffect, useState } from "react";
+import { User } from "../../models/User";
+import { Button, Card, Form, InputGroup, ListGroup } from "react-bootstrap";
+import "./UpdateProfilePage.css";
+import Spinner from "react-bootstrap/Spinner";
+
+interface UpdateValues {
+  alias: UpdateValue<string>;
+}
+
+interface UpdateValue<TValue> {
+  value: TValue;
+  changed: boolean;
+}
+
+export default function UpdateProfilePage() {
+  const { get } = useApi();
+  const [userInfo, setUserInfo] = useState<User>();
+  const navigate = useNavigate();
+  useEffect(getUserInfo, []);
+  useEffect(loadUserInfo, [userInfo]);
+
+  const [updateValues, setUpdateValues] = useState<UpdateValues>();
+
+  if (!updateValues) {
+    return (
+      <div className="spinner-container">
+        <Spinner animation="border" variant="primary" />
+      </div>
+    );
+  }
+
+  return (
+    <div className="update-profile-form-container">
+      <Card border="primary">
+        <Card.Header>Edit</Card.Header>
+        <Card.Body>
+          <ListGroup>
+            <ListGroup.Item>
+              <Form.Label
+                className={`profile-field-label ${
+                  updateValues.alias.changed ? "changed" : ""
+                }`}
+              >
+                Alias{updateValues.alias.changed ? "*" : ""}
+              </Form.Label>
+              <Form.Control
+                value={updateValues.alias.value}
+                onChange={(event) =>
+                  setUpdateValues((values) => ({
+                    ...values,
+                    alias: {
+                      value: event.target.value,
+                      changed: (userInfo?.alias ?? "") !== event.target.value,
+                    },
+                  }))
+                }
+              />
+            </ListGroup.Item>
+          </ListGroup>
+          <Button
+            className="profile-update-save-button"
+            variant="primary"
+            disabled={!canSave()}
+          >
+            Save
+          </Button>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+
+  function getUserInfo() {
+    get<User>("users/account")
+      .then((response) => setUserInfo(response.data))
+      .catch(() => navigate(-1));
+  }
+
+  function loadUserInfo() {
+    if (!userInfo) {
+      return;
+    }
+
+    setUpdateValues({
+      alias: { value: userInfo.alias, changed: false },
+    });
+  }
+
+  function canSave() {
+    return (
+      updateValues &&
+      Object.values(updateValues).findIndex((value) => value.changed) !== -1
+    );
+  }
+}
