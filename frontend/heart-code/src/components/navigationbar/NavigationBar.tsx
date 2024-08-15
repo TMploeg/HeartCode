4116ebc514217c7d26893b435450481f8bc68107
TMploeg <82577971+TMploeg@users.noreply.github.com>
2024-08-15 12:04:49 +0200

    updated nav bar

--- frontend/heart-code/src/components/navigationbar/NavigationBar.tsx
+++ frontend/heart-code/src/components/navigationbar/NavigationBar.tsx
@@ -1,21 +1,13 @@
 import NavigationBarButton from "./NavigationBarButton";
 import "./NavigationBar.css";
-import {
-  BsFillPersonFill,
-  BsChat,
-  BsHouseDoorFill,
-  BsPersonPlusFill,
-  BsFilePersonFill,
-} from "react-icons/bs";
+import { BsFillPersonFill, BsChat, BsSearch } from "react-icons/bs";
 
 export default function NavigationBar() {
   return (
     <div className="nav-bar">
       <NavigationBarButton page="account" icon={<BsFillPersonFill />} />
-      <NavigationBarButton page="" icon={<BsHouseDoorFill />} />
+      <NavigationBarButton page="" icon={<BsSearch />} />
       <NavigationBarButton page="matches" icon={<BsChat />} />
-      <NavigationBarButton page="register" icon={<BsPersonPlusFill />} />
-      <NavigationBarButton page="login" icon={<BsFilePersonFill />} />
     </div>
   );
 }
