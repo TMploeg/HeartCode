0499713da2fd9102e2fe7acd99403bac7632b0c6
EGerritsen99 <145434680+EGerritsen99@users.noreply.github.com>
2024-08-15 10:59:01 +0200

    cleanup

--- frontend/heart-code/src/components/auth/AuthValidation.ts
+++ frontend/heart-code/src/components/auth/AuthValidation.ts
@@ -12,45 +12,24 @@ function isValidPassword(password: string): boolean {
     let numOfSpecialChars = 0;
     for (let i = 0; i < password.length; i++) {
       let ch = password.charAt(i);
-      if (password.includes("!@#$%^&*()-_=+[{]};:'\",<.>/?\\|")) {
-        console.log(ch + " is special char");
+      let format = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+      if (format.test(ch)) {
         numOfSpecialChars++;
       } else if (/^\d+$/.test(ch)) {
-        console.log(ch + " is digit");
         numOfDigits++;
       } else if (ch === ch.toUpperCase()) {
-        console.log(ch + " is uppercase");
         numOfUppercase++;
       } else if (ch === ch.toLowerCase()) {
-        console.log(ch + " is lowercase");
         numOfLowercase++;
       }
     }
-    if (
+
+    return (
       numOfUppercase >= 1 &&
       numOfLowercase >= 1 &&
       numOfDigits >= 1 &&
       numOfSpecialChars >= 1
-    ) {
-      console.log("password valid");
-      console.log(
-        numOfDigits,
-        numOfLowercase,
-        numOfSpecialChars,
-        numOfUppercase
-      );
-      return true;
-    } else {
-      console.log("password not valid");
-      console.log(
-        numOfDigits,
-        numOfLowercase,
-        numOfSpecialChars,
-        numOfUppercase
-      );
-
-      return false;
-    }
+    );
   }
 }
 
