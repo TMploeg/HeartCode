74045e4f9662d5bf14ba490ca52cceffcf16db98
TMploeg <82577971+TMploeg@users.noreply.github.com>
2024-08-19 13:59:34 +0200

    moved login/register endpoints into AuthController

--- backend/heartcode/src/main/java/com/itvitae/heartcode/user/UserController.java
+++ backend/heartcode/src/main/java/com/itvitae/heartcode/user/UserController.java
@@ -1,9 +1,7 @@
 package com.itvitae.heartcode.user;
 
 import com.itvitae.heartcode.exceptions.BadRequestException;
-import com.itvitae.heartcode.profilepictures.ProfilePicture;
 import com.itvitae.heartcode.profilepictures.ProfilePictureService;
-import com.itvitae.heartcode.security.AuthTokenDTO;
 import com.itvitae.heartcode.security.JwtService;
 import jakarta.transaction.Transactional;
 import java.util.*;
@@ -21,77 +19,6 @@ public class UserController {
   private final UserService userService;
   private final JwtService jwtService;
   private final ProfilePictureService profilePictureService;
-
-  @PostMapping("register")
-  public AuthTokenDTO register(@RequestBody RegisterDTO registerDTO) {
-    if (registerDTO.email() == null) {
-      throw new BadRequestException("email is required");
-    } else if (userService.isInvalidEmail(registerDTO.email())
-        || userService.userWithEmailExists(registerDTO.email())) {
-      throw new BadRequestException("email is invalid");
-    }
-    if (registerDTO.alias() == null || registerDTO.alias().isBlank()) {
-      throw new BadRequestException("alias is required");
-    }
-    if (registerDTO.password() == null || !userService.isValidPassword(registerDTO.password())) {
-      throw new BadRequestException("password is not valid");
-    }
-
-    if (registerDTO.gender() == null) {
-      throw new BadRequestException("gender is required");
-    } else if (registerDTO.gender().isBlank()) {
-      throw new BadRequestException("gender must have at least one character");
-    }
-
-    if (registerDTO.profilePicture() == null) {
-      throw new BadRequestException("profilePicture is required");
-    } else if (registerDTO.profilePicture().isEmpty()) {
-      throw new BadRequestException("profilePicture is empty");
-    }
-
-    UserGender gender =
-        UserGender.parse(registerDTO.gender())
-            .orElseThrow(
-                () ->
-                    new BadRequestException(
-                        "gender is invalid, valid options include: ["
-                            + getGenderOptionsString()
-                            + "]"));
-
-    ProfilePicture profilePicture = profilePictureService.save(registerDTO.profilePicture());
-
-    User user =
-        userService.save(
-            registerDTO.email(),
-            registerDTO.alias(),
-            registerDTO.password(),
-            gender,
-            profilePicture);
-
-    return new AuthTokenDTO(
-        jwtService
-            .generateTokenForUser(user.getEmail())
-            .orElseThrow(
-                () -> new RuntimeException("could not generate token for unknown reasons")));
-  }
-
-  @PostMapping("login")
-  public AuthTokenDTO login(@RequestBody LoginDTO authDTO) {
-    if (authDTO.email() == null || authDTO.password() == null) {
-      throw new BadRequestException("email and password are required");
-    }
-
-    userService
-        .findById(authDTO.email())
-        .filter(user -> userService.isCorrectPassword(user, authDTO.password()))
-        .orElseThrow(() -> new BadRequestException("username or password is incorrect"));
-
-    return new AuthTokenDTO(
-        jwtService
-            .generateTokenForUser(authDTO.email())
-            .orElseThrow(
-                () -> new RuntimeException("could not generate token for unknown reasons")));
-  }
 
   @GetMapping("account")
   public ResponseEntity<UserDTO> getCurrentUser() {
