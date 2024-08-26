3cdbb55944900585dda837d49af983bc36af240a
TMploeg <82577971+TMploeg@users.noreply.github.com>
2024-08-26 13:06:14 +0200

    removed UserSpecification

--- backend/heartcode/src/main/java/com/itvitae/heartcode/user/UserRepository.java
+++ backend/heartcode/src/main/java/com/itvitae/heartcode/user/UserRepository.java
@@ -3,12 +3,11 @@ package com.itvitae.heartcode.user;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface UserRepository
-    extends JpaRepository<User, String>, JpaSpecificationExecutor<User> {
+    extends JpaRepository<User, String> {
   String USER_ALIAS = "u";
   String EVALUATION_ALIAS = "e";
 
